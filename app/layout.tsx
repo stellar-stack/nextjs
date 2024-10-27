083b536693e308ff586916892f66d7513401b999
elliot <elliotgould5721@gmail.com>
2024-10-28 00:29:00 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <h1 className="text-3xl">ROOT </h1>
         {children}
       </body>
     </html>
